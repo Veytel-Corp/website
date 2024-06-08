f5770422febc35914841d7a9041d9af8c2f50f12
AFMVeytel <aidanmeyer@veytel.com>
2024-06-07 19:51:48 -0700

    Added basic design and functionality of header + nav bar
    Added header design for pc view. I will do mobile after the landing page is complete.

--- js/load_header.js
+++ js/load_header.js
@@ -1,3 +1,10 @@
+(function loadHeaderStyles(filePath = './css/header.css') {
+    const headEl = document.querySelector('head');
+    let headElInnerHTML = headEl.innerHTML;
+    headElInnerHTML += `<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"> <link rel="stylesheet" href="./css/header.css" />`;
+    headEl.innerHTML = headElInnerHTML;
+})();
+
 (function loadHeader(filePath = './html/header.html') {
     fetch(filePath)
         .then(response => {
@@ -7,7 +14,7 @@
             return response.text()
         })
         .then(data => {
-            document.querySelector("header").innerHTML = data;
+            document.querySelector('header').innerHTML = data;
     })
     .catch(error => {
         console.error(`Something went wrong when fetching the header content: `, error);
