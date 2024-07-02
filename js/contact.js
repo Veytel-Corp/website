0d0764ba32137916889015cdddb3fdcacd895402
AFMVeytel <AFMVeytel@users.noreply.github.com>
2024-07-02 23:19:27 +0000

    deploy: bf19ce3d7b08ec4b0c342497c2e577250f3846de

--- js/contact.js
+++ js/contact.js
@@ -13,8 +13,9 @@ formEl.addEventListener('submit', async (e)=> {
         const formObject = Object.fromEntries(formData.entries());
 
         try {
-            const requestURL = "https://script.google.com/macros/s/AKfycbyWLGWbuyX_AXNWlYqCo6CLBTRhPBR1jrUlYFadbEgn066m-gF5lgDQmFZEr_3y7GYrRw/exec"
-            loaderEl.classList.remove('hidden')
+            // const requestURL = "https://script.google.com/macros/s/AKfycbyWLGWbuyX_AXNWlYqCo6CLBTRhPBR1jrUlYFadbEgn066m-gF5lgDQmFZEr_3y7GYrRw/exec";
+            const requestURL = "https://script.google.com/macros/s/AKfycby22ij9ioLxPK8tXGpKyJy5j_eqDyXHpoDZPVpLMv7XtRvT7WjqZ88sY_Uyw539Ygzl/exec";
+            loaderEl.classList.remove('hidden');
             const response = await fetch(requestURL, {
                 method: 'POST',
                 body: new URLSearchParams(formObject)
