9242bd2d295eb028cfff1ceb528be094f3af1fda
AFMVeytel <AFMVeytel@users.noreply.github.com>
2024-07-08 15:25:17 +0000

    deploy: 5eae54494e0b0aed39c76b1b94742245e4098348

--- staging/js/team.js
+++ staging/js/team.js
@@ -1,14 +1,30 @@
-function parallaxEffect(element, speedFactor = 0.5) {
-    if (!element) return;
-    
-    window.addEventListener('scroll', function() {
-        var scrollPosition = window.scrollY || document.documentElement.scrollTop;
-        element.style.transform = 'translateY(' + (scrollPosition * speedFactor) + 'px)';
-    });
-};
+(function env() {
 
-// Usage
-document.addEventListener('DOMContentLoaded', () => {
-    const parallaxElement = document.querySelector('#team-landing-text-con');
-    parallaxEffect(parallaxElement, -0.25);
-});
+    function parallaxEffect(element, speedFactor = 0.5) {
+        if (!element) return;
+
+        let lastScrollY = window.scrollY;
+        let ticking = false;
+
+        function onScroll() {
+            lastScrollY = window.scrollY;
+            if (!ticking) {
+                window.requestAnimationFrame(updateParallax);
+                ticking = true;
+            }
+        }
+
+        function updateParallax() {
+            element.style.transform = 'translateY(' + (lastScrollY * speedFactor) + 'px)';
+            ticking = false;
+        }
+
+        window.addEventListener('scroll', onScroll);
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const pittsburghEl = document.querySelector('#team-hero-background');
+        parallaxEffect(pittsburghEl, 0.15);
+    });
+
+})()
