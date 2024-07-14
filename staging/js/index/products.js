cf69014f4d0bbc0a5ca4f943db56a0f3ff7d5456
AFMVeytel <AFMVeytel@users.noreply.github.com>
2024-07-14 23:41:57 +0000

    deploy: 17a256dd545bd5be270b23bfebdf9246ff946360

---
+++ staging/js/index/products.js
@@ -0,0 +1,13 @@
+const productButtons = document.querySelectorAll('.product-button-con');
+
+productButtons.forEach((con) => {
+    const productName = con.dataset.productname;
+    const reqDemBtnEl = con.querySelector('.request-demo-button');
+    reqDemBtnEl.addEventListener('click', ()=> {
+
+    })
+});
+
+function toggleRequestDemoModal() {
+    
+}
