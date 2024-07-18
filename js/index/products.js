a295d508293be52e56ee93697fe44e4504fc7aec
AFMVeytel <aidanmeyer@veytel.com>
2024-07-18 15:57:32 -0500

    Refactored products js

--- js/index/products.js
+++ js/index/products.js
@@ -1,89 +1,138 @@
-const formEl = document.querySelector('.request-demo-form'); 
-const thankYouEl = document.querySelector('.request-demo-success');
-const somethingWentWrongEl = document.querySelector('.request-demo-fail');
-const loaderEl = document.querySelector('.loader-con');
-const exitModalEl = document.querySelector('.exit-btn');
-
-const closeReqDemModalElements = document.querySelectorAll('.close-request-demo-modal');
-const productButtons = document.querySelectorAll('.product-button-con');
-const discoverMoreButtons = document.querySelectorAll('.discover-more-button');
-
-// DISCOVER MORE
-discoverMoreButtons.forEach((btn) => {
-    const productName = btn.parentElement.dataset.productname;
-    btn.addEventListener('click', ()=> {
-        sendToProductsPage(productName);
-    });
-})
-
-function sendToProductsPage(productName) {
-    window.location.assign(`/products.html?productname=${productName}`);
+const productBtnsManager = {
+    productsPageURL: window.location.href.includes('staging') ? '/staging/products.html' : '/products.html',
+    targetProduct: '',
+}
+productBtnsManager.getTargetURL = () => {
+    const targetProduct = productBtnsManager.targetProduct;
+    const productsPageURL = productBtnsManager.productsPageURL;
+    return `${productsPageURL}?productname=${targetProduct}`;
+}
+productBtnsManager.sendToProductsPage = () => {
+    const targetURL = productBtnsManager.getTargetURL();
+    window.location.assign(targetURL);
+}
+productBtnsManager.initDiscoverMoreBtn = () => {
+    const discoverMoreBtns = document.querySelectorAll('.discover-more-button');
+    discoverMoreBtns.forEach((btn) => {
+        const productName = btn.parentElement.dataset.productname;
+        btn.addEventListener('click', ()=> {
+            productBtnsManager.targetProduct = productName;
+            productBtnsManager.sendToProductsPage();
+        });
+    })
 }
 
-// REQUEST DEMO
-const productInfo = {
-    productName: "",
+const requestDemoFormManager = {
+    requestDemoModalEl: document.querySelector('.request-demo-modal'),
+    formEl: document.querySelector('.request-demo-form'),
+    thankYouEl: document.querySelector('.request-demo-success'),
+    somethingWentWrongEl: document.querySelector('.request-demo-fail'),
+    loaderEl: document.querySelector('.loader-con'),
+    exitModalEl: document.querySelector('.exit-btn'),
+    requestURL: 'https://script.google.com/macros/s/AKfycby22ij9ioLxPK8tXGpKyJy5j_eqDyXHpoDZPVpLMv7XtRvT7WjqZ88sY_Uyw539Ygzl/exec',
+    targetProduct: '',
+    formData: null,
 }
-
-// Handle form submit
-formEl.addEventListener('submit', async (e)=> {
-    e.preventDefault();
-
+requestDemoFormManager.toggleFormView = () => {
+    const requestDemoModalEl = requestDemoFormManager.requestDemoModalEl;
+    requestDemoModalEl.classList.toggle('hidden');
+}
+requestDemoFormManager.setFormData = () => {
+    const targetProduct = requestDemoFormManager.targetProduct;
+    const formEl = requestDemoFormManager.formEl;
     const formData = new FormData(formEl);
-    formData.set("name", `Request Demo for ${productInfo.productName}`);
-    formData.set("message", `I would like to know more about the product ${productInfo.productName}.`);
-    const formObject = Object.fromEntries(formData.entries());
-
+    formData.set("name", `Request Demo for ${targetProduct}`);
+    formData.set("message", `I would like to know more about the product ${targetProduct}.`);
+    requestDemoFormManager.formData = formData;
+}
+requestDemoFormManager.getFormDataObj = () => {
+    const formData = requestDemoFormManager.formData;
+    return Object.fromEntries(formData.entries());
+}
+requestDemoFormManager.submitForm = async (e) => {
+    e.preventDefault();
+    requestDemoFormManager.setFormData();
+    const formDataObj = requestDemoFormManager.getFormDataObj();
     try {
-        const requestURL = "https://script.google.com/macros/s/AKfycby22ij9ioLxPK8tXGpKyJy5j_eqDyXHpoDZPVpLMv7XtRvT7WjqZ88sY_Uyw539Ygzl/exec";
+        const requestURL = requestDemoFormManager.requestURL;
+        const loaderEl = requestDemoFormManager.loaderEl;
         loaderEl.classList.remove('hidden');
         const response = await fetch(requestURL, {
             method: 'POST',
-            body: new URLSearchParams(formObject)
+            body: new URLSearchParams(formDataObj)
         });
         const data = await response.json();
-
         if (data.status === 'success') {
-            formEl.reset();
-            exitModalEl.classList.remove('hidden');
-            formEl.classList.add('hidden');
-            thankYouEl.classList.remove('hidden');
-            loaderEl.classList.add('hidden');
+            requestDemoFormManager.successSubmitDOMChange();
+
         }
-    } catch (err) {
-        console.error(err);
-        exitModalEl.classList.remove('hidden');
-        formEl.classList.add('hidden');
-        loaderEl.classList.add('hidden');
-        somethingWentWrongEl.classList.remove('hidden');
     }
-});
+    catch (err) {
+        requestDemoFormManager.failSubmitDOMChange(err);
+    }
 
-productButtons.forEach((con) => {
-    const reqDemBtnEl = con.querySelector('.request-demo-button');
-    reqDemBtnEl.addEventListener('click', ()=>{
-        productInfo.productName = con.dataset.productname;
-        toggleRequestDemoModal();
-    })
-});
-
-// Attach event listener to close the modal whenever the element is clicked.
-// Calls resetRequestDemoModal
-closeReqDemModalElements.forEach((btn)=> {
-    btn.addEventListener('click', resetRequestDemoModal);
-});
-
-// Toggles hidden class from request demo modal
-function toggleRequestDemoModal() {
-    document.querySelector('.request-demo-modal').classList.toggle('hidden');
 }
+requestDemoFormManager.successSubmitDOMChange = () => {
+    const formEl = requestDemoFormManager.formEl;
+    const thankYouEl = requestDemoFormManager.thankYouEl;
+    const loaderEl = requestDemoFormManager.loaderEl;
+    const exitModalEl = requestDemoFormManager.exitModalEl;
 
-// Hides anything that should be hidden, resets form, calls toggleRequestDemoModal
-function resetRequestDemoModal() {
-    if (!exitModalEl.classList.contains('hidden')) exitModalEl.classList.add('hidden');
-    if (!thankYouEl.classList.contains('hidden')) thankYouEl.classList.add('hidden');
-    if (!somethingWentWrongEl.classList.contains('hidden')) somethingWentWrongEl.classList.add('hidden');
+    formEl.reset();
+    exitModalEl.classList.remove('hidden');
+    thankYouEl.classList.remove('hidden');
+    formEl.classList.add('hidden');
+    loaderEl.classList.add('hidden');
+}
+requestDemoFormManager.failSubmitDOMChange = (err) => {
+    console.log(err);
+    const formEl = requestDemoFormManager.formEl;
+    const somethingWentWrongEl = requestDemoFormManager.somethingWentWrongEl;
+    const loaderEl = requestDemoFormManager.loaderEl;
+    const exitModalEl = requestDemoFormManager.exitModalEl;
+
+    exitModalEl.classList.remove('hidden');
+    somethingWentWrongEl.classList.remove('hidden');
+    formEl.classList.add('hidden');
+    loaderEl.classList.add('hidden');
+}
+requestDemoFormManager.resetModalView = () => {
+    const exitModalEl = requestDemoFormManager.exitModalEl;
+    const thankYouEl = requestDemoFormManager.thankYouEl;
+    const somethingWentWrongEl = requestDemoFormManager.somethingWentWrongEl;
+    const formEl = requestDemoFormManager.formEl;
+
+    exitModalEl.classList.add('hidden');
+    thankYouEl.classList.add('hidden');
+    somethingWentWrongEl.classList.add('hidden');
     formEl.classList.remove('hidden');
     formEl.reset();
-    toggleRequestDemoModal();
+    requestDemoFormManager.toggleFormView();
 }
+requestDemoFormManager.initRequestDemoBtn = () => {
+    const productButtonCons = document.querySelectorAll('.product-button-con');
+    productButtonCons.forEach((con) => {
+        const reqDemBtnEl = con.querySelector('.request-demo-button');
+        reqDemBtnEl.addEventListener('click', ()=>{
+            requestDemoFormManager.targetProduct = con.dataset.productname;
+            requestDemoFormManager.toggleFormView();
+        })
+    })
+}
+requestDemoFormManager.initFormSumit = () => {
+    const formEl = requestDemoFormManager.formEl;
+    formEl.addEventListener('submit', requestDemoFormManager.submitForm)
+}
+requestDemoFormManager.initCloseRequestDemoModalBtn = () => {
+    const closeRequestDemoModalEl = document.querySelectorAll('.close-request-demo-modal');
+    closeRequestDemoModalEl.forEach((btn)=> {
+        btn.addEventListener('click', requestDemoFormManager.resetModalView);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    productBtnsManager.initDiscoverMoreBtn();
+    requestDemoFormManager.initRequestDemoBtn();
+    requestDemoFormManager.initFormSumit();
+    requestDemoFormManager.initCloseRequestDemoModalBtn();
+});
